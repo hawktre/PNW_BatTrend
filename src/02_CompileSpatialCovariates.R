d728fd543fe2182372dd28c7ed8fb5c3de7ec868
Laura McMahon <126629110+lamcmahon@users.noreply.github.com>
2026-08-31 15:37:47 -0700

    got 2025 na_bat covars included, but variable names are different

--- src/02_CompileSpatialCovariates.R
+++ src/02_CompileSpatialCovariates.R
@@ -13,18 +13,24 @@ library(terra)
 
 # Read in data ------------------------------------------------------------
 ## nabat covariates already aggregated for Udell et al., 2022
-nabat_covars <- st_read(
-  dsn = here("data/raw/covariates/NABat_grid_covariates/"),
-  layer = "NABat_grid_covariates"
-)
+# nabat_covars <- st_read(
+#   dsn = here("data/raw/covariates/NABat_grid_covariates/"),
+#   layer = "NABat_grid_covariates"
+# )
+#
 ## conus_grts key
 # conus10k <- read_sf(here(
 #   "data/raw/batgrid/complete_conus_mastersample_10km_attributed.shp"
 # ))
 
+# Revise with 2025 (not sure this is right or the best way to do this)
+# 2025 data (https://www.sciencebase.gov/catalog/item/67eafce2d34ed02007f83722)
+nabat_covars <- read_csv(
+  "data/raw/covariates/NABat_grid_covariates/Conus_10km_covs.csv",
+  show_col_types = FALSE)
+
 conus10k <- read_sf(here(
-  "data/raw/batgrid/conus_mastersample_10km_attributed.shp"
-))
+  "data/raw/batgrid/conus_mastersample_10km_attributed.shp"))
 
 ##Landfire gap cover
 landfire_or <- terra::rast(here(
@@ -49,15 +55,44 @@ nabat_pnw <- nabat_covars %>%
 
 # Join to get CONUS -------------------------------------------------------
 
+# conus_grts_key <- conus10k %>%
+#   select(CONUS_10KM, GRTS_ID)
+# 
+# ## Join with conus and do some formatting
+# conus_pnw_covars <- nabat_pnw %>%
+#   left_join(
+#     as.data.frame(conus_grts_key) %>% select(-geometry),
+#     by = "GRTS_ID"
+#   ) %>%
+#   select(
+#     CONUS_10KM,
+#     GRTS_ID,
+#     "state" = admin1,
+#     long,
+#     lat,
+#     karst,
+#     p_forest,
+#     p_wetland,
+#     mean_temp,
+#     precip,
+#     DEM_max,
+#     physio_div,
+#     dist_mines,
+#     riverlake,
+#     geometry
+#   )
+
+
 conus_grts_key <- conus10k %>%
   select(CONUS_10KM, GRTS_ID)
 
 ## Join with conus and do some formatting
 conus_pnw_covars <- nabat_pnw %>%
   left_join(
-    as.data.frame(conus_grts_key) %>% select(-geometry),
-    by = "GRTS_ID"
-  ) %>%
+    conus10k %>%
+      select(CONUS_10KM, GRTS_ID, lat, long, geometry),
+    by = "CONUS_10KM") %>%
+  st_as_sf() %>% 
   select(
     CONUS_10KM,
     GRTS_ID,
@@ -65,21 +100,31 @@ conus_pnw_covars <- nabat_pnw %>%
     long,
     lat,
     karst,
-    p_forest,
-    p_wetland,
-    mean_temp,
-    precip,
+    prop_forest_2005,
+    prop_forest_2010,
+    prop_forest_2015,
+    prop_forest_2020,
+    prop_wetland_2005,
+    prop_wetland_2010,
+    prop_wetland_2015,
+    prop_wetland_2020,
+    temp_mean,
+    precip_mean,
     DEM_max,
-    physio_div,
+    phys_div,
     dist_mines,
-    riverlake,
+    prop_water_2005,
+    prop_water_2010,
+    prop_water_2015,
+    prop_water_2020,
     geometry
   )
 
 ## rename to make it easier to call
 covars <- conus_pnw_covars
-plot(covars["physio_div"])
-plot(covars["state"])
+plot(covars["phys_div"])
+plot(covars["state"]) ## why are there plots that don't look like they're in the right state?
+
 # Get Cliff_Canyon ---------------------------------------------------------------
 ## Create a single layer
 landfire <- terra::merge(landfire_or, landfire_wa, landfire_id, first = T)
